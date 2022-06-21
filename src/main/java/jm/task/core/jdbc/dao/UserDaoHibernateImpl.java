44be3be32c156527816cfa3fd03fffa30cae1c27
Виталий Корнилов <ratlion@rambler.ru>
2022-06-21 13:33:45 +0300

    Использован try with resources, добавлен rollback

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -4,6 +4,7 @@ import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 import java.util.List;
 
@@ -11,7 +12,6 @@ import java.util.List;
 public class UserDaoHibernateImpl implements UserDao {
     private final SessionFactory sessionFactory;
     private final String TABLE = "users";
-    private Session session;
 
     public UserDaoHibernateImpl() {
         sessionFactory = Util.getSessionFactory();
@@ -25,25 +25,29 @@ public class UserDaoHibernateImpl implements UserDao {
                 "name VARCHAR(30) NOT NULL, " +
                 "lastName VARCHAR(30) NOT NULL, " +
                 "age TINYINT NOT NULL)";
-        try {
-            session = sessionFactory.openSession();
-            session.beginTransaction();
-            session.createSQLQuery(sql).executeUpdate();
-            session.getTransaction().commit();
-        } finally {
-            if (session != null && session.isOpen()) session.close();
+        try (Session session = sessionFactory.openSession()) {
+            Transaction transaction = session.beginTransaction();
+            try {
+                session.createSQLQuery(sql).executeUpdate();
+                transaction.commit();
+            } catch (Exception e) {
+                if (transaction.isActive()) transaction.rollback();
+                throw e;
+            }
         }
     }
 
     @Override
     public void dropUsersTable() {
-        try {
-            session = sessionFactory.openSession();
-            session.beginTransaction();
-            session.createSQLQuery("DROP TABLE IF EXISTS " + TABLE).executeUpdate();
-            session.getTransaction().commit();
-        } finally {
-            if (session != null && session.isOpen()) session.close();
+        try (Session session = sessionFactory.openSession()) {
+            Transaction transaction = session.beginTransaction();
+            try {
+                session.createSQLQuery("DROP TABLE IF EXISTS " + TABLE).executeUpdate();
+                transaction.commit();
+            } catch (Exception e) {
+                if (transaction.isActive()) transaction.rollback();
+                throw e;
+            }
         }
     }
 
@@ -51,54 +55,62 @@ public class UserDaoHibernateImpl implements UserDao {
     public void saveUser(String name, String lastName, byte age) {
         String sql = String.format("INSERT %s(name, lastName, age) VALUES ('%s', '%s', '%d')",
                 TABLE, name, lastName, age);
-        try {
-            session = sessionFactory.openSession();
-            session.beginTransaction();
-            session.createSQLQuery(sql).executeUpdate();
-            session.getTransaction().commit();
-        } finally {
-            if (session != null && session.isOpen()) session.close();
+        try (Session session = sessionFactory.openSession()) {
+            Transaction transaction = session.beginTransaction();
+            try {
+                session.createSQLQuery(sql).executeUpdate();
+                transaction.commit();
+            } catch (Exception e) {
+                if (transaction.isActive()) transaction.rollback();
+                throw e;
+            }
         }
     }
 
     @Override
     public void removeUserById(long id) {
         String sql = "DELETE FROM " + TABLE + " WHERE id=" + id;
-        try {
-            session = sessionFactory.openSession();
-            session.beginTransaction();
-            session.createSQLQuery(sql).executeUpdate();
-            session.getTransaction().commit();
-        } finally {
-            if (session != null && session.isOpen()) session.close();
+        try (Session session = sessionFactory.openSession()) {
+            Transaction transaction = session.beginTransaction();
+            try {
+                session.createSQLQuery(sql).executeUpdate();
+                transaction.commit();
+            } catch (Exception e) {
+                if (transaction.isActive()) transaction.rollback();
+                throw e;
+            }
         }
     }
 
     @Override
     public List<User> getAllUsers() {
         List<User> users;
-        try {
-            session = sessionFactory.openSession();
-            session.beginTransaction();
-            users = (List<User>) session.createSQLQuery("SELECT * FROM " + TABLE)
-                    .addEntity(User.class)
-                    .list();
-            session.getTransaction().commit();
-        } finally {
-            if (session != null && session.isOpen()) session.close();
+        try (Session session = sessionFactory.openSession()) {
+            Transaction transaction = session.beginTransaction();
+            try {
+                users = (List<User>) session.createSQLQuery("SELECT * FROM " + TABLE)
+                        .addEntity(User.class)
+                        .list();
+                transaction.commit();
+            } catch (Exception e) {
+                if (transaction.isActive()) transaction.rollback();
+                throw e;
+            }
         }
         return users;
     }
 
     @Override
     public void cleanUsersTable() {
-        try {
-            session = sessionFactory.openSession();
-            session.beginTransaction();
-            session.createSQLQuery("DELETE FROM " + TABLE).executeUpdate();
-            session.getTransaction().commit();
-        } finally {
-            if (session != null && session.isOpen()) session.close();
+        try (Session session = sessionFactory.openSession()) {
+            Transaction transaction = session.beginTransaction();
+            try {
+                session.createSQLQuery("DELETE FROM " + TABLE).executeUpdate();
+                transaction.commit();
+            } catch (Exception e) {
+                if (transaction.isActive()) transaction.rollback();
+                throw e;
+            }
         }
     }
 }
